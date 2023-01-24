91dd05bbcd4e9e1fcc9f1e99b0a7b752f8e946fd
Fynn Westerath <westerathf@gmail.com>
2023-01-24 09:05:23 +0100

    create application class

---
+++ src/main/java/ch/fwesterath/socialmedia/Application.java
@@ -0,0 +1,17 @@
+package ch.fwesterath.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Application
+ * @author Fynn Westerath
+ * @version 23.01.2022
+ */
+@SpringBootApplication
+public class Application {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+   }
+
+}
